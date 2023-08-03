a528aeea951240ff77c099e88535cd8a759d62db
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-08-03 15:51:12 -0300

    Módulo 2 concluído

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -2,6 +2,7 @@ import {
   animate,
   group,
   keyframes,
+  query,
   state,
   style,
   transition,
@@ -72,76 +73,119 @@ export const filterTrigger = trigger('filterAnimation', [
     animate(
       '2000ms ease-out',
       keyframes([
-        style({ offset: 0 ,opacity: 0, width: 0 }),
-        style({ offset: 0.8, opacity: 0.5, width: '*', backgroundColor: 'lightgreen' }),
-        style({ offset: 1,opacity: 1, width: '*',  backgroundColor: 'lightblue'  }),
+        style({ offset: 0, opacity: 0, width: 0 }),
+        style({
+          offset: 0.8,
+          opacity: 0.5,
+          width: '*',
+          backgroundColor: 'lightgreen',
+        }),
+        style({
+          offset: 1,
+          opacity: 1,
+          width: '*',
+          backgroundColor: 'lightblue',
+        }),
       ])
     ),
   ]),
   transition(':leave', [
     animate(
       '400ms cubic-bezier(.13,.9,.8,.1)',
-      style({opacity: 0, width: 0,})
+      style({ opacity: 0, width: 0 })
     ),
   ]),
 ]);
 
 export const formButtonTrigger = trigger('formButton', [
   transition('invalid => valid', [
-    group([
-      animate(200, style({
-        backgroundColor: '#63B77C'
-      })),
-      animate(100, style({
-       transform: 'scale(1.1)'
-      })),
+    query('#botao-salvar', [
+      group([
+        animate(
+          200,
+          style({
+            backgroundColor: '#63B77C',
+          })
+        ),
+        animate(
+          100,
+          style({
+            transform: 'scale(1.1)',
+          })
+        ),
+      ]),
+      animate(
+        200,
+        style({
+          transform: 'scale(1)',
+        })
+      ),
     ]),
-    animate(200, style({
-      transform: 'scale(1)'
-    }))
   ]),
   transition('valid => invalid', [
-    group([
-      animate(200, style({
-        backgroundColor: '#6C757D'
-      })),
-      animate(100, style({
-       transform: 'scale(1.1)'
-      })),
+    query('#botao-salvar', [
+      group([
+        animate(
+          200,
+          style({
+            backgroundColor: '#6C757D',
+          })
+        ),
+        animate(
+          100,
+          style({
+            transform: 'scale(1.1)',
+          })
+        ),
+      ]),
+      animate(
+        200,
+        style({
+          transform: 'scale(1)',
+        })
+      ),
     ]),
-    animate(200, style({
-      transform: 'scale(1)'
-    }))
-  ])
-])
+  ]),
+]);
 
-export const flyInOutTrigger =
-  trigger('flyInOut', [
-    transition(':enter', [
-      style({
-        width: '100%',
-        transform: 'translateX(-100%)',
-        opacity: 0
-      }),
-      group([
-        animate('0.3s 0.1s ease', style({
+export const flyInOutTrigger = trigger('flyInOut', [
+  transition(':enter', [
+    style({
+      width: '100%',
+      transform: 'translateX(-100%)',
+      opacity: 0,
+    }),
+    group([
+      animate(
+        '0.3s 0.1s ease',
+        style({
           transform: 'translateX(0)',
-          width: '*'
-        })),
-        animate('0.3s ease', style({
-          opacity: 1
-        }))
-      ])
+          width: '*',
+        })
+      ),
+      animate(
+        '0.3s ease',
+        style({
+          opacity: 1,
+        })
+      ),
     ]),
-    transition(':leave', [
-      group([
-        animate('0.3s ease', style({
+  ]),
+  transition(':leave', [
+    group([
+      animate(
+        '0.3s ease',
+        style({
           transform: 'translateX(100%)',
-          width: '*'
-        })),
-        animate('0.3s 0.2s ease', style({
-          opacity: 0
-        }))
-      ])
-    ])
-  ])
+          width: '*',
+        })
+      ),
+      animate(
+        '0.3s 0.2s ease',
+        style({
+          opacity: 0,
+        })
+      ),
+    ]),
+  ]),
+]);

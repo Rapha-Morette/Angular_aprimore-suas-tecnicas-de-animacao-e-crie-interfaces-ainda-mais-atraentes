666ee559ca940ed6733024e10d0343519b3b63df
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-08-04 11:34:59 -0300

    Módulo 4 video 2 concluído

--- src/app/service/tarefa.service.ts
+++ src/app/service/tarefa.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 import { Tarefa } from '../interface/tarefa';
 
@@ -9,17 +9,21 @@ import { Tarefa } from '../interface/tarefa';
 })
 export class TarefaService {
   private readonly API = 'http://localhost:3000/tarefas';
+  private tarefasSubject = new BehaviorSubject<Tarefa[]>([]);
+  tarefas$ = this.tarefasSubject.asObservable()
+
   constructor(private http: HttpClient) {}
 
-  listar(categoria: string): Observable<Tarefa[]> {
+  listar(): void {
     let params = new HttpParams().appendAll({
       _sort: 'id',
       _order: 'desc',
     });
-    if (categoria) {
-      params = params.append('categoria', categoria);
-    }
-    return this.http.get<Tarefa[]>(this.API, { params });
+    this.http.get<Tarefa[]>(this.API, { params }).subscribe((tarefas) => {
+      let tarefasTemporarias = this.tarefasSubject.getValue()
+      tarefasTemporarias = tarefasTemporarias.concat(tarefas)
+      this.tarefasSubject.next(tarefasTemporarias)
+    });
   }
 
   criar(tarefa: Tarefa): Observable<Tarefa> {
